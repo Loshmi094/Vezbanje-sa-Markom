7b07c8d2849da1220c68e9826ed7dc013ceee070
Loshmi094 <122690872+Loshmi094@users.noreply.github.com>
2023-03-18 18:48:20 +0100

    Add files via upload

---
+++ Mi radnici.py
@@ -0,0 +1,27 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 10:41:24) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Milos-Python/AppData/Local/Programs/Python/Python37/test za Marka.py
+thank you. Your total is : $2500
+>>> 
+= RESTART: C:/Users/Milos-Python/AppData/Local/Programs/Python/Python37/test za Marka.py
+Marko23
+>>> 
+= RESTART: C:/Users/Milos-Python/AppData/Local/Programs/Python/Python37/test za Marka.py
+Traceback (most recent call last):
+  File "C:/Users/Milos-Python/AppData/Local/Programs/Python/Python37/test za Marka.py", line 5, in <module>
+    print (name+age+p)
+TypeError: can only concatenate str (not "int") to str
+>>> 
+= RESTART: C:/Users/Milos-Python/AppData/Local/Programs/Python/Python37/test za Marka.py
+Milos ima platu od 260000
+njegov kolega Dejan ima platu od 590000
+Tu je i Marko sa platom od 480000
+svi imaju jedan kljucni problem a to je porez koji skida 50000.Uracunavajuci sve 3 plate sa porezom iznosi: $1180000 neto sa sve 3 plate zajedno.U svakom slucaju. 
+
+Milos nema pare za ps5 Dejan nema pare za teslu, a Marko nema pare za trotinet koji je na snizenju :(.
+>>> 
+= RESTART: C:/Users/Milos-Python/AppData/Local/Programs/Python/Python37/test za Marka.py
+Prosek plate: 77333.33333333333
+Prosek godina: 34.666666666666664
+>>> 
